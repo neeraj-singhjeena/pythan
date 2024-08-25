5bc3b56ba2e625996225597aa9fb5026287fc0bb
neeraj-singhjeena <151987844+neeraj-singhjeena@users.noreply.github.com>
2024-08-25 19:35:21 +0530

    pattern

---
+++ pyton 2.py/pattern.py
@@ -0,0 +1,8 @@
+raw = input("enter the number of columns:")
+row = input("enter the number of raw:")
+for i in range(int(row)):
+    for j in range(int(i)):
+        print("x", end=" ")
+    print()
+
+
