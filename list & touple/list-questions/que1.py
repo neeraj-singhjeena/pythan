3bd99d279cc3ed59988a59bc66bf52a765caa305
Neeraj jeena <neeraj2005jeena@gmail.com>
2024-09-02 22:50:55 +0530

    this is the section of list and tuples

---
+++ list & touple/list-questions/que1.py
@@ -0,0 +1,29 @@
+
+#list name is name
+
+name = ["car","bike","helicopter"]
+print(name)
+print(type(name))
+print(name.append("aroplane"))
+print(name)
+print(name.sort())
+print(name)
+print(name.pop(0))
+print(name)
+print(name.insert(2,"macleran"))
+print(name)
+print(name.reverse())
+print(name)
+
+
+
+# or
+movies = []
+mov1 = input("enter 1st movie:")
+mov2 = input("enter 2nd movie:")
+mov3 = input("enter 3rd movie:")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
---
+++ list & touple/tuples-question/que3.py
@@ -0,0 +1,9 @@
+tuple = ["c","D","A","A","B","B","A"]
+tuple.sort()
+print(tuple)
+
+print("numbers of A is:",tuple.count("A"))
+
+#accending order
+# tuple.sort()
+# print(tuple)
